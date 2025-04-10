a4eff50698f565cba3d8a4f5bb563f581f79a54d
deleenananaa <deleenarana4@gmail.com>
2025-04-10 21:48:02 +0545

    updated home and settigs page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -53,58 +53,58 @@ class SettingsPage extends StatelessWidget {
             Expanded(
               child: ListView(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 16, top: 16, bottom: 8),
-                    child: Text(
-                      'General Settings',
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFFFFFF),
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withValues(alpha: .2),
-                          spreadRadius: 1,
-                          blurRadius: 3,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: const Icon(
-                            Icons.lock_outline,
-                            color: Colors.grey,
-                          ),
-                          title: const Text('Change Password'),
-                          trailing: const Icon(Icons.chevron_right),
-                          onTap: () {
-                            // Handle password change
-                          },
-                        ),
-                        const Divider(height: 1),
-                        ListTile(
-                          leading: const Icon(
-                            Icons.notifications_none,
-                            color: Colors.grey,
-                          ),
-                          title: const Text('Notification Preferences'),
-                          trailing: const Icon(Icons.chevron_right),
-                          onTap: () {
-                            // Handle notification preferences
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
+                  // const Padding(
+                  //   padding: EdgeInsets.only(left: 16, top: 16, bottom: 8),
+                  //   child: Text(
+                  //     'General Settings',
+                  //     style: TextStyle(color: Colors.grey, fontSize: 16),
+                  //   ),
+                  // ),
+                  // Container(
+                  //   margin: const EdgeInsets.symmetric(
+                  //     horizontal: 16,
+                  //     vertical: 8,
+                  //   ),
+                  //   decoration: BoxDecoration(
+                  //     color: Color(0xFFFFFFFF),
+                  //     borderRadius: BorderRadius.circular(12),
+                  //     boxShadow: [
+                  //       BoxShadow(
+                  //         color: Colors.grey.withValues(alpha: .2),
+                  //         spreadRadius: 1,
+                  //         blurRadius: 3,
+                  //         offset: const Offset(0, 2),
+                  //       ),
+                  //     ],
+                  //   ),
+                  //   child: Column(
+                  //     children: [
+                  //       ListTile(
+                  //         leading: const Icon(
+                  //           Icons.lock_outline,
+                  //           color: Colors.grey,
+                  //         ),
+                  //         title: const Text('Change Password'),
+                  //         trailing: const Icon(Icons.chevron_right),
+                  //         onTap: () {
+                  //           // Handle password change
+                  //         },
+                  //       ),
+                  //       const Divider(height: 1),
+                  //       ListTile(
+                  //         leading: const Icon(
+                  //           Icons.notifications_none,
+                  //           color: Colors.grey,
+                  //         ),
+                  //         title: const Text('Notification Preferences'),
+                  //         trailing: const Icon(Icons.chevron_right),
+                  //         onTap: () {
+                  //           // Handle notification preferences
+                  //         },
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                   const Padding(
                     padding: EdgeInsets.only(left: 16, top: 16, bottom: 8),
                     child: Text(
