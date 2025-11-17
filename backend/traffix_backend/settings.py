3aacf6ee3e6a072c313b30d7d3b5ab3997cd9a57
Deleena Rana <119421004+deleenananaa@users.noreply.github.com>
2025-11-17 12:39:34 +0545

    Update database user and password defaults

--- backend/traffix_backend/settings.py
+++ backend/traffix_backend/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'traffix_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'USER': os.getenv('DB_USER', 'username'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
